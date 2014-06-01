04733226fcc8ba38fd80b4a53b133a751b7e2785
Artem Polikarpov <artpolikarpov@gmail.com>
2014-06-01 17:12:14 +0400

    Добавил config-sample.js

---
+++ copies-sample.js
@@ -0,0 +1,8 @@
+// Переименуйте этот файл в copies.js и отредактируйте пути,
+// если вы хотите, чтобы Грант скопировал тему в разные установки Эгеи.
+// Удобно редактировать тему в одном месте и тут же проверять в разном окружении
+
+module.exports = [
+  '/Users/artpolikarpov/Projects/My/e2/',
+  '/Users/artpolikarpov/Projects/My/artpolikarpov.ru/www/'
+];
